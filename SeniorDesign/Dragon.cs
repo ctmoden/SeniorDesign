c09befba6a22c5b723006746c8ed25de126770de
Christian Moden <ctmoden@ksu.edu>
2022-04-04 09:16:59 -0600

    Added dragon assets and started dragon class and game state enum

---
+++ SeniorDesign/Dragon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace SeniorDesign
+{
+    public class Dragon
+    {
+        
+    }
+}
---
+++ SeniorDesign/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeniorDesign
+{
+    public enum GameState
+    {
+        LoadScreen,
+        Level1,
+        Level2
+    }
+}
