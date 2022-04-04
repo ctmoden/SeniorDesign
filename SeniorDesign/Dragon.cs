8ae28f06cdf8f7c2de241cd2d065f7134ccbbdd8
Christian Moden <ctmoden@ksu.edu>
2022-04-04 09:28:33 -0600

    question for the dragon class: I want multiple dragons, how do I make multiple instances with different textures and difficulties the most clean/efficiently?  Inheritance?   Load all textures right off the start for sure, create and object and pass in a game state level to tailor dragon object to level

--- SeniorDesign/Dragon.cs
+++ SeniorDesign/Dragon.cs
@@ -11,6 +11,39 @@ namespace SeniorDesign
 {
     public class Dragon
     {
-        
+        private Texture2D dragonTexture;
+
+        private double animationTimer;
+
+        private const short ANIMATION_ROW = 2;
+
+        private Vector2 position;
+
+        //FIXME need a public position?
+
+        private int hitCount;
+
+        public bool Alive;
+
+        public Dragon()
+        {
+            hitCount = 0;
+            position = new Vector2(500, 500);
+
+        }
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
